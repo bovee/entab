7ba43e66e9b1af85d3a9ad0110b2f3604fd009be
Roderick <rbovee@gmail.com>
2020-07-04 21:43:42 -0700

    Add JS bindings

---
+++ entab-js/src/lib.rs
@@ -0,0 +1,187 @@
+mod utils;
+
+use std::collections::HashMap;
+use std::io::{Cursor, Read};
+
+use entab::buffer::ReadBuffer;
+use entab::compression::decompress;
+use entab::readers::get_builder;
+use entab::record::{Record as EtRecord, RecordReader};
+use entab::utils::error::EtError;
+use serde::Serialize;
+use wasm_bindgen::prelude::*;
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[derive(Serialize)]
+pub struct NextRecord {
+    value: Option<Record>,
+    done: bool,
+}
+
+#[derive(Serialize)]
+#[serde(untagged)]
+pub enum Record {
+    Mz {
+        time: f64,
+        mz: f64,
+        intensity: u64,
+    },
+    MzFloat {
+        time: f64,
+        mz: f64,
+        intensity: f64,
+    },
+    Sam {
+        query_name: String,
+        flag: u16,
+        ref_name: String,
+        pos: Option<u64>,
+        mapq: Option<u8>,
+        cigar: String,
+        rnext: String,
+        pnext: Option<u32>,
+        tlen: i32,
+        seq: String,
+        qual: String,
+        extra: String,
+    },
+    Sequence {
+        id: String,
+        sequence: String,
+        quality: Option<String>,
+    },
+    Tsv(HashMap<String, String>),
+}
+
+fn to_owned_rec(rec: EtRecord) -> Result<Record, EtError> {
+    Ok(match rec {
+        EtRecord::Mz {
+            time,
+            mz,
+            intensity,
+        } => Record::Mz {
+            time,
+            mz,
+            intensity,
+        },
+        EtRecord::MzFloat {
+            time,
+            mz,
+            intensity,
+        } => Record::MzFloat {
+            time,
+            mz,
+            intensity,
+        },
+        EtRecord::Sam {
+            query_name,
+            flag,
+            ref_name,
+            pos,
+            mapq,
+            cigar,
+            rnext,
+            pnext,
+            tlen,
+            seq,
+            qual,
+            extra,
+        } => Record::Sam {
+            query_name: query_name.to_string(),
+            flag,
+            ref_name: ref_name.to_string(),
+            pos,
+            mapq,
+            cigar: String::from_utf8(cigar.to_vec())?,
+            rnext: rnext.to_string(),
+            pnext,
+            tlen,
+            seq: String::from_utf8(seq.to_vec())?,
+            qual: String::from_utf8(qual.to_vec())?,
+            extra: String::from_utf8(extra.to_vec())?,
+        },
+        EtRecord::Sequence {
+            id,
+            sequence,
+            quality,
+        } => {
+            let quality = if let Some(q) = quality {
+                Some(String::from_utf8(q.to_vec())?)
+            } else {
+                None
+            };
+            Record::Sequence {
+                id: id.to_string(),
+                sequence: String::from_utf8(sequence.to_vec())?,
+                quality,
+            }
+        },
+        EtRecord::Tsv(recs, headers) => {
+            let map = headers.iter().zip(recs.iter()).map(|(k, v)| (k.clone(), v.to_string())).collect();
+            Record::Tsv(map)
+        }
+    })
+}
+
+#[wasm_bindgen]
+pub struct Reader {
+    parser: String,
+    reader: Box<dyn RecordReader>,
+}
+
+fn to_js(err: EtError) -> JsValue {
+    err.to_string().into()
+}
+
+#[wasm_bindgen]
+impl Reader {
+    #[wasm_bindgen(constructor)]
+    pub fn new(data: Box<[u8]>, parser: Option<String>) -> Result<Reader, JsValue> {
+        utils::set_panic_hook();
+
+        let stream: Box<dyn Read> = Box::new(Cursor::new(data));
+
+        let (reader, filetype, _) = decompress(stream).map_err(to_js)?;
+        let buffer = ReadBuffer::new(reader).map_err(to_js)?;
+
+        let parser_name = parser.unwrap_or(filetype.to_parser_name().to_string());
+        let reader = if let Some(builder) = get_builder(&parser_name) {
+            builder.to_reader(buffer).map_err(to_js)?
+        } else {
+            return Err(JsValue::from_str("No reader could be found for this file type"));
+        };
+        Ok(Reader {
+            parser: parser_name.to_string(),
+            reader,
+        })
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn parser(&self) -> String {
+        self.parser.clone()
+    }
+
+    // FIXME: it'd be nice to implement iterable
+    // #[wasm_bindgen(js_name = "@@iterable")]
+    // pub fn iterable(&self) -> JsValue {
+    //     self
+    // }
+
+    #[wasm_bindgen]
+    pub fn next(&mut self) -> Result<JsValue, JsValue> {
+        if let Some(value) = self.reader.next().map_err(to_js)? {
+            JsValue::from_serde(&NextRecord {
+                value: Some(to_owned_rec(value).map_err(to_js)?),
+                done: false,
+            }).map_err(|_| JsValue::from_str("Error translating record"))
+        } else {
+            JsValue::from_serde(&NextRecord {
+                value: None,
+                done: false,
+            }).map_err(|_| JsValue::from_str("Error translating record"))
+        }
+    }
+}
---
+++ entab-js/tests/web.rs
@@ -0,0 +1,31 @@
+//! Test suite for the Web and headless browsers.
+
+#![cfg(target_arch = "wasm32")]
+
+use entab_js::Reader;
+use js_sys::{Object, Reflect};
+use wasm_bindgen::{JsCast, JsValue};
+use wasm_bindgen_test::*;
+
+wasm_bindgen_test_configure!(run_in_browser);
+
+#[wasm_bindgen_test]
+fn create_reader() {
+    // doesn't work for obvious reasons, but it'd be nice to test against a Uint8Array
+    // let data = Uint8Array::new(&JsValue::from_str(">test\nACGT"));
+    let data = b">test\nACGT";
+    let mut reader = Reader::new(data.to_vec().into_boxed_slice(), None).expect("Error creating the reader");
+    assert_eq!(reader.parser(), "fasta");
+    let raw_rec = reader.next().expect("Error reading first record");
+    let rec = raw_rec.dyn_into::<Object>().expect("next() returns an object");
+
+    let done = Reflect::get(&rec, &JsValue::from_str("done")).expect("record has done");
+    assert!(done.is_falsy());
+
+    let raw_value = Reflect::get(&rec, &JsValue::from_str("value")).expect("record has value");
+    let value = raw_value.dyn_into::<Object>().expect("value is an object");
+    let raw_id = Reflect::get(&value, &JsValue::from_str("id")).expect("value has id");
+    assert_eq!(raw_id, JsValue::from_str("test"));
+    assert!(Reflect::has(&value, &JsValue::from_str("sequence")).expect("value has sequence"));
+    assert!(Reflect::has(&value, &JsValue::from_str("quality")).expect("value has quality"));
+}
