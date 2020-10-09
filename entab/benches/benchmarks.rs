4577f01e6c5177e6ff1247a381d4fafe55f3a9db
Roderick <rbovee@gmail.com>
2020-10-08 21:08:51 -0700

    Add more benchmarks

--- entab/benches/benchmarks.rs
+++ entab/benches/benchmarks.rs
@@ -6,10 +6,14 @@ use entab::buffer::ReadBuffer;
 use entab::readers::chemstation::ChemstationMsReader;
 use entab::readers::fasta::FastaReader;
 use entab::readers::fastq::FastqReader;
+use entab::readers::png::PngReader;
 use entab::readers::get_reader;
 
-fn benchmark_readers(c: &mut Criterion) {
-    c.bench_function("chemstation reader", |b| {
+fn benchmark_raw_readers(c: &mut Criterion) {
+    let mut raw_readers = c.benchmark_group("raw readers");
+    raw_readers.significance_level(0.01).sample_size(500);
+
+    raw_readers.bench_function("chemstation reader", |b| {
         b.iter(|| {
             let f = File::open("tests/data/carotenoid_extract.d/MSD1.MS").unwrap();
             let rb = ReadBuffer::new(Box::new(&f)).unwrap();
@@ -20,7 +24,7 @@ fn benchmark_readers(c: &mut Criterion) {
         })
     });
 
-    c.bench_function("fasta reader", |b| {
+    raw_readers.bench_function("fasta reader", |b| {
         b.iter(|| {
             let f = File::open("tests/data/sequence.fasta").unwrap();
             let rb = ReadBuffer::new(Box::new(&f)).unwrap();
@@ -31,7 +35,7 @@ fn benchmark_readers(c: &mut Criterion) {
         })
     });
 
-    c.bench_function("fastq reader", |b| {
+    raw_readers.bench_function("fastq reader", |b| {
         b.iter(|| {
             let f = File::open("tests/data/test.fastq").unwrap();
             let rb = ReadBuffer::new(Box::new(&f)).unwrap();
@@ -42,7 +46,33 @@ fn benchmark_readers(c: &mut Criterion) {
         })
     });
 
-    c.bench_function("generic fastq reader", |b| {
+    raw_readers.bench_function("png reader", |b| {
+        b.iter(|| {
+            let f = File::open("tests/data/bmp_24.png").unwrap();
+            let rb = ReadBuffer::new(Box::new(&f)).unwrap();
+            let mut reader = PngReader::new(rb, ()).unwrap();
+            while let Some(record) = reader.next().unwrap() {
+                black_box(record);
+            }
+        })
+    });
+}
+
+fn benchmark_generic_readers(c: &mut Criterion) {
+    let mut generic_readers = c.benchmark_group("generic readers");
+    generic_readers.significance_level(0.01).sample_size(500);
+    generic_readers.bench_function("generic chemstation reader", |b| {
+        b.iter(|| {
+            let f = File::open("tests/data/carotenoid_extract.d/MSD1.MS").unwrap();
+            let rb = ReadBuffer::new(Box::new(&f)).unwrap();
+            let mut reader = get_reader("chemstation_ms", rb).unwrap();
+            while let Some(record) = reader.next_record().unwrap() {
+                black_box(record);
+            }
+        })
+    });
+
+    generic_readers.bench_function("generic fastq reader", |b| {
         b.iter(|| {
             let f = File::open("tests/data/test.fastq").unwrap();
             let rb = ReadBuffer::new(Box::new(&f)).unwrap();
@@ -52,7 +82,29 @@ fn benchmark_readers(c: &mut Criterion) {
             }
         })
     });
+
+    generic_readers.bench_function("flow reader", |b| {
+        b.iter(|| {
+            let f = File::open("tests/data/HTS_BD_LSR_II_Mixed_Specimen_001_D6_D06.fcs").unwrap();
+            let rb = ReadBuffer::new(Box::new(&f)).unwrap();
+            let mut reader = get_reader("fcs", rb).unwrap();
+            while let Some(record) = reader.next_record().unwrap() {
+                black_box(record);
+            }
+        })
+    });
+
+    generic_readers.bench_function("png reader", |b| {
+        b.iter(|| {
+            let f = File::open("tests/data/bmp_24.png").unwrap();
+            let rb = ReadBuffer::new(Box::new(&f)).unwrap();
+            let mut reader = get_reader("png", rb).unwrap();
+            while let Some(record) = reader.next_record().unwrap() {
+                black_box(record);
+            }
+        })
+    });
 }
 
-criterion_group!(benches, benchmark_readers);
+criterion_group!(benches, benchmark_raw_readers, benchmark_generic_readers);
 criterion_main!(benches);
