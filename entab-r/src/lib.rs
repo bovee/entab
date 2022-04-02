80ad4ebfbee93b1a2a054dc4c188febc9751be3f
Roderick <rbovee@gmail.com>
2022-04-02 12:35:48 -0700

    Rewrite `as.data.frame` to create specialized Vecs

--- entab-r/src/lib.rs
+++ entab-r/src/lib.rs
@@ -98,23 +98,64 @@ impl Reader {
     }
 }
 
+pub enum ValueList {
+    Null(usize),
+    Boolean(Vec<bool>),
+    Float(Vec<f64>),
+    Integer(Vec<i64>),
+    String(Vec<String>),
+    Misc(Vec<Robj>),
+}
+
 #[extendr]
 fn as_data_frame(reader: &mut Reader) -> Result<Robj> {
-    let mut data: Vec<Vec<Robj>> = vec![vec![]; reader.header_names.len()];
-    while let Some(record) = reader.reader.next_record().map_err(to_r)? {
-        for (ix, v) in record.into_iter().enumerate() {
-            data[ix].push(value_to_robj(v));
+    let mut data: Vec<ValueList> = Vec::new();
+    if let Some(first) = reader.reader.next_record().map_err(to_r)? {
+        for v in first {
+            data.push(match v {
+                Value::Null => ValueList::Null(1),
+                Value::Boolean(b) => ValueList::Boolean(vec![b]),
+                Value::Float(f) => ValueList::Float(vec![f]),
+                Value::Integer(i) => ValueList::Integer(vec![i]),
+                Value::String(s) => ValueList::String(vec![s.to_string()]),
+                x => ValueList::Misc(vec![value_to_robj(x)]),
+            });
+        }
+        while let Some(record) = reader.reader.next_record().map_err(to_r)? {
+            for (ix, v) in record.into_iter().enumerate() {
+                match (&mut data[ix], v) {
+                    (ValueList::Null(x), Value::Null) => *x += 1,
+                    (ValueList::Boolean(v), Value::Boolean(b)) => v.push(b),
+                    (ValueList::Float(v), Value::Float(f)) => v.push(f),
+                    (ValueList::Integer(v), Value::Integer(i)) => v.push(i),
+                    (ValueList::String(v), Value::String(s)) => v.push(s.to_string()),
+                    (ValueList::Misc(v), x) => v.push(value_to_robj(x)),
+                    _ => panic!("Tried to append wrong data type"),
+                }
+            }
+        }
+    } else {
+        for _ in &reader.header_names {
+            data.push(ValueList::Null(0));
         }
     }
+
     let mut vectors: Vec<Robj> = vec![];
     for v in data {
-        vectors.push(v.into());
+        vectors.push(match v {
+            ValueList::Null(x) => vec![r!(NULL); x].into(),
+            ValueList::Boolean(v) => v.iter().collect_robj(),
+            ValueList::Float(v) => v.iter().collect_robj(),
+            ValueList::Integer(v) => v.iter().collect_robj(),
+            ValueList::String(v) => v.iter().collect_robj(),
+            ValueList::Misc(v) => v.into(),
+        });
     }
     let obj: Robj = List::from_names_and_values(&reader.header_names, &vectors).into();
     obj.set_attrib(
         row_names_symbol(),
         (1i32..=vectors[0].len() as i32).collect_robj(),
-        )?;
+    )?;
     obj.set_class(&["data.frame"])?;
     Ok(obj)
 }
