4f0e7153f7a8bcf85fa12715f7ffd79d53e24ca4
Roderick <rbovee@gmail.com>
2024-10-17 20:00:30 -0700

    Fix service worker not installing issue

--- entab-js/example/service-worker.js
+++ entab-js/example/service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open('entab').then(cache => {
-      return cache.add('/index.html').then(() => self.skipWaiting());
+      return cache.add('./index.html').then(() => self.skipWaiting());
     })
   )
 });
@@ -10,10 +10,10 @@ self.addEventListener('activate', evt => {
   evt.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", evt => {
+self.addEventListener('fetch', evt => {
   // fix for the bug here: https://bugs.chromium.org/p/chromium/issues/detail?id=823392
-  if (evt.request.cache === "only-if-cached" && evt.request.mode !== "same-origin") {
-    return
+  if (evt.request.cache === 'only-if-cached' && evt.request.mode !== 'same-origin') {
+    return;
   }
 
   evt.respondWith(
